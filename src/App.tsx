27853531421a62fcbf0a5e1ee75c81c4aa0b5477
Matteo <62598618+matnut2@users.noreply.github.com>
2023-05-22 18:06:46 +0200

    First upload

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react';
+import './App.css';
+
+export function App() {
+  return (
+    <body>
+      <header><h1>Lumos Minima</h1></header>
+      <nav>Home</nav>
+      <main>
+        <h2>Lampioni Collegati</h2>
+      </main>
+      <footer>
+        4ourSquared Team <br></br>
+        Università degli Studi di Padova<br></br>
+        Progetto Didattico di SWE
+        </footer>
+    </body>
+  );
+}
