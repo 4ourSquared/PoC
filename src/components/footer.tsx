264593f9e6ea40ed9c082dcf96c481a4c18ac136
matnut2 <matteosolda12@gmail.com>
2023-05-23 10:41:16 +0200

    Creazione classe footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+export class Footer extends Component{
+    render(){
+        return(
+            <footer>
+                4ourSquared Team <br/>
+                Università degli Studi di Padova <br/>
+                Progetto Didattico di SWE
+            </footer>
+        )
+    }
+}
