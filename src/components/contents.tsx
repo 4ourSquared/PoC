c5bd8672dca8ed457310e6f41ff599b8586c2f4b
matnut2 <matteosolda12@gmail.com>
2023-05-23 12:31:04 +0200

    Aggiunta cards all'applicazione + UI

--- src/components/contents.tsx
+++ src/components/contents.tsx
@@ -1,10 +1,30 @@
 import React, {Component} from 'react'
+import { LampioneCard } from './LampioneCard'
 
 export class Content extends Component{
     render(){
         return(
             <main>
-
+                <h2>Lampioni Collegati</h2>
+                <div className='row'>
+                    <LampioneCard />
+                    <LampioneCard />
+                    <LampioneCard />
+                    <LampioneCard />
+                    <LampioneCard />
+                    <LampioneCard />
+                    <LampioneCard />
+                    <LampioneCard />
+                    <LampioneCard />
+                    <LampioneCard />
+                    <LampioneCard />
+                    <LampioneCard />
+                    <LampioneCard />
+                    <LampioneCard />
+                    <LampioneCard />
+                    <LampioneCard />
+                    <LampioneCard />
+                </div>
             </main>
         )
     }
