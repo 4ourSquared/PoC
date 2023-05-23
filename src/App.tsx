98d8c59bb24dce71e841cbcaedf38c3dba9dcb54
matnut2 <matteosolda12@gmail.com>
2023-05-23 10:59:15 +0200

    Aggiunta del content all'applicazione

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import './App.css';
 
 // Importazione Componenti
 import {Header} from './components/breadcrumb';
+import {Content} from './components/contents';
 import {Footer} from './components/footer';
 
 export function App(){
   return(
     <>
       <Header />
+      <Content />
       <Footer />
     </>
   )
