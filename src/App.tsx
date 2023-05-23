de3ef4ba9fde17c411475a03dc83c105b6fb75f5
matnut2 <matteosolda12@gmail.com>
2023-05-23 10:41:49 +0200

    aggiunta del footer all'applicazione

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,14 @@ import './App.css';
 
 // Importazione Componenti
 import {Header} from './components/breadcrumb';
-
-/*
-export function App() {
-  return (
-    <body>
-      <header><h1>Lumos Minima</h1></header>
-      <nav id="breadcrumb">Home</nav>
-      <main>
-        <h2>Lampioni Collegati</h2>
-        <div id="lamp-box">
-          [placeholder-lampioni]
-        </div>
-      </main>
-      <footer>
-        4ourSquared Team <br></br>
-        Università degli Studi di Padova<br></br>
-        Progetto Didattico di SWE
-        </footer>
-    </body>
-  );
-}
-*/
+import {Footer} from './components/footer';
 
 export function App(){
   return(
-    <Header />
+    <>
+      <Header />
+      <Footer />
+    </>
   )
 }
 
