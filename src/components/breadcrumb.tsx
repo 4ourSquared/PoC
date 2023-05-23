08c625083b097097a55e4b8d1287edbc1edaf01a
matnut2 <matteosolda12@gmail.com>
2023-05-23 10:41:34 +0200

    sistemazione header

--- src/components/breadcrumb.tsx
+++ src/components/breadcrumb.tsx
@@ -1,18 +1,24 @@
-function Breadcrumb(){
-    return(
-        <nav aria-label="breadcrumb">
-            <ol className="breadcrumb">
-                <li className="breadcrumb-item active" aria-current="page">Home</li>
-            </ol>
-        </nav>
-    )
+import React, {Component} from 'react'
+
+class Breadcrumb extends Component{
+    render(){
+        return(
+            <nav aria-label="breadcrumb">
+                <ol className="breadcrumb">
+                    <li className="breadcrumb-item active" aria-current="page">Home</li>
+                </ol>
+            </nav>
+        )
+    }
 }
 
-export function Header(){
-return(
-    <header>
-        <h1>Lumos Minima</h1>
-        <Breadcrumb />
-    </header>
-)
+export class Header extends Component{
+    render(){
+        return(
+            <header>
+                <h1>Lumos Minima</h1>
+                <Breadcrumb />
+            </header>
+        )
+    }
 }
