972660cbc007bc4b3e1e6aa36638903b48f8c50b
matnut2 <matteosolda12@gmail.com>
2023-05-24 17:12:37 +0200

    Aggiunta pulsanti info + UI

--- src/components/LampioneTable.tsx
+++ src/components/LampioneTable.tsx
@@ -4,13 +4,14 @@ export class LampioneTable extends Component{
     render(){
         return(
             <div className="row justify-content-center">
-                <table className="table table-hover" style={{width: '90%' }}>
+                <table className="table table-hover align-middle" style={{width: '90%' }}>
                     <thead>
                         <tr>
                         <th scope="col">ID</th>
                         <th scope="col">Stato</th>
                         <th scope="col">Intensità</th>
                         <th scope="col">Zona Illuminata</th>
+                        <th scope="col">Info</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -19,18 +20,21 @@ export class LampioneTable extends Component{
                         <td style={{backgroundColor: 'yellow'}}>ON</td>
                         <td>5</td>
                         <td>Piazza Mazzini</td>
+                        <td><button type="button" className="btn btn-outline-info">Info</button></td>
                         </tr>
                         <tr>
                         <th scope="row">2</th>
                         <td style={{backgroundColor: 'black', color:'white'}}>OFF</td>
                         <td>8</td>
                         <td>Via Crescente</td>
+                        <td><button type="button" className="btn btn-outline-info">Info</button></td>
                         </tr>
                         <tr>
                         <th scope="row">3</th>
                         <td style={{backgroundColor: 'yellow'}}>ON</td>
                         <td>7</td>
                         <td>Navigli</td>
+                        <td><button type="button" className="btn btn-outline-info">Info</button></td>
                         </tr>
                     </tbody>
                 </table>
