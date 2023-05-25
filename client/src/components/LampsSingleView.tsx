1627f41f90c84227661171c14a496711cbbaa46d
nicolasalberti00 <alberti.nicolas00@gmail.com>
2023-05-25 17:37:45 +0200

    Prima aggiunta pagina lampione singolo; inserito routing fittizio pagina lampione

---
+++ client/src/components/LampsSingleView.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { Header } from "./breadcrumb";
+import { Content } from "./contents";
+import { Footer } from "./footer";
+
+export class LampsSingleView extends Component {
+  render() {
+    return (
+      <>
+        <Header />
+
+        <Footer />
+      </>
+    );
+  }
+}
--- client/src/routing/routing.tsx
+++ client/src/routing/routing.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { LampsFullView } from '../components/LampsFullView';
+import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { LampsFullView } from "../components/LampsFullView";
+import { LampsSingleView } from "../components/LampsSingleView";
 
-export const RouterComponent : React.FC = () => {
-    return(
-        <Router>
-            <Routes>
-                <Route path="/" Component={LampsFullView}/>
-            </Routes>
-        </Router>
-    )
-}
+export const RouterComponent: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" Component={LampsFullView} />
+        <Route path="/lampione" Component={LampsSingleView} />
+      </Routes>
+    </Router>
+  );
+};
