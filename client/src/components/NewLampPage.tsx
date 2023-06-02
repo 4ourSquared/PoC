4e5c044316bec8554ce34661d177022ce1c8e336
matnut2 <matteosolda12@gmail.com>
2023-06-02 17:26:13 +0200

    commentato NewLampPage

--- client/src/components/NewLampPage.tsx
+++ client/src/components/NewLampPage.tsx
@@ -3,6 +3,9 @@ import { NewLampForm } from "./NewLampForm";
 import { Header } from "./breadcrumb";
 import { Footer } from "./footer";
 
+/*
+  CLASS NEWLAMPPAGE: classe che renderizza automaticamente l'intera pagina contenente il form per l'aggiunta di un nuovo lampione.
+*/
 export class NewLampPage extends Component {
   render() {
     return (
