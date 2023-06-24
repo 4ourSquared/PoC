09373273558d7347d59ad44e78e27bfda5709b46
EricaCavaliere <erica.cavaliere.euganeoinf@gmail.com>
2023-06-24 12:21:07 +0200

    Creata pagina di modifica sensore
    Problemi:
    - la pagina risulta vuota quando viene richiamata
    - è stata creata senza usare il form Formik (dava errori in compilazione)

--- client/src/components/EditSensForm.tsx
+++ client/src/components/EditSensForm.tsx
@@ -1,14 +1,32 @@
 import axios from "axios";
-import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import SensItem from "../types/SensItem";
 
 /*
-  CLASSE NEWSENSFORM: classe che renderizza automaticamente la struttura HTML della pagina di aggiunta di un sensore, definendo anche il metodo per la trasmissione dei dati al server. Stile associato a Bootstrap.
-  ATTENZIONE: Bisogna ancora implementare l'avanzamento automatico del campo ID affinchè tale chiave sia univoca per l'identificazione del sensore.
+  CLASSE EDITFORM: classe che renderizza automaticamente la struttura HTML della pagina di modifica di un lampione, definendo anche il metodo per la trasmissione dei dati al server. Stile associato a Bootstrap.
 */
-const NewSensForm: React.FC = () => {
-  axios.defaults.baseURL = "http://localhost:5000/api"; //URL base, così una volta in produzione basta cambiare questo
+
+const EditSensForm: React.FC = () => {
   const navigate = useNavigate();
+  const [sens, setSens] = useState<SensItem | null>(null);
+  const { id } = useParams<{ id: string }>(); // Accesso al parametro id passandolo a useParams()
+
+  useEffect(() => {
+    fetchData();
+  }, []); //Anche se il compilatore da warning, mantenere l'array vuoto per evitare loop infiniti, dato che
+  //effettua la richiesta ad ogni render della pagina
+
+  const fetchData = async () => {
+    axios.defaults.baseURL = "http://localhost:5000/api";
+    try {
+      const response = await axios.get<SensItem>(`sensori/edit/${id}`);
+      setSens(response.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault(); // Evita l'invio di un form di default
     const formElement = event.target as HTMLFormElement; // Individuo il form nella pagina HTML
@@ -24,8 +42,9 @@ const NewSensForm: React.FC = () => {
   };
 
   return (
-    <div className="content">
-      <form title="Form per inserimento nuovo sensore" onSubmit={handleSubmit}>
+    <div>
+      {sens ? (
+        <form title="Form per modifica sensore" onSubmit={handleSubmit}>
         <div className="form-group">
           <label htmlFor="id">ID (Automatico)</label>
           <input
@@ -33,12 +52,13 @@ const NewSensForm: React.FC = () => {
             type="text"
             defaultValue="NULL"
             name="id"
+            value={sens.id}
             readOnly
           />
         </div>
         <div className="form-group">
           <label htmlFor="iter">Interazione</label>
-          <select className="form-control" id="iter" name="iter">
+          <select className="form-control" id="iter" name="iter" value={sens.iter === "manuale" ? "Manuale" : "Automatico"}>
             <option>Manuale</option>
             <option>Automatico</option>
           </select>
@@ -54,6 +74,7 @@ const NewSensForm: React.FC = () => {
             id="IP"
             placeholder=""
             name="IP"
+            value={sens.IP}
           />
         </div>
         <div className="form-group">
@@ -65,6 +86,7 @@ const NewSensForm: React.FC = () => {
             aria-describedby="locazioneHelp"
             placeholder=""
             name="luogo"
+            value={sens.luogo}
           />
           <small id="locazioneHelp" className="form-text text-muted">
             Indica il luogo in cui è situato il sensore.
@@ -72,7 +94,7 @@ const NewSensForm: React.FC = () => {
         </div>
         <div className="form-group">
           <label htmlFor="raggio">Raggio d'azione</label>
-          <select className="form-control" id="raggio" name="raggio">
+          <select className="form-control" id="raggio" name="raggio" value={sens.raggio}>
             <option>0</option>
             <option>1</option>
             <option>2</option>
@@ -90,16 +112,16 @@ const NewSensForm: React.FC = () => {
           </small>
         </div>
         <button type="submit" className="btn btn-primary">
-          Crea
-        </button>
-        <button type="reset" className="btn btn-secondary">
-          Resetta
+          Modifica
         </button>
         <Link to="/" type="button" className="btn btn-outline-primary">
           Indietro
         </Link>
       </form>
+      ) : (
+        <p>Nessun dato disponibile</p>
+      )}
     </div>
   );
 };
-export default NewSensForm;
+export default EditSensForm;
