3ac822c2362964ea77db1d8f7e4aa969c37ee47a
EricaCavaliere <erica.cavaliere.euganeoinf@gmail.com>
2023-06-26 15:20:04 +0200

    Aggiunto il controllo dell'indirizzo IP

--- client/src/components/NewSensForm.tsx
+++ client/src/components/NewSensForm.tsx
@@ -54,6 +54,8 @@ const NewSensForm: React.FC = () => {
             id="IP"
             placeholder=""
             name="IP"
+            pattern="(?:(?:2(?:[0-4][0-9]|5[0-5])|[0-1]?[0-9]?[0-9])\.){3}(?:(?:2([0-4][0-9]|5[0-5])|[0-1]?[0-9]?[0-9]))"
+            title="indirizzo IP valido per esempio 0.0.0.0"
           />
         </div>
         <div className="form-group">
