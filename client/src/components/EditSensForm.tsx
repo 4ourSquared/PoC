8e489bd230f82b13e6ce641a5eb0545d326275dc
LSalamiUnipd <lorenzo.salami@studenti.unipd.it>
2023-06-27 20:07:58 +0200

    Aggiornato EditSensForm

--- client/src/components/EditSensForm.tsx
+++ client/src/components/EditSensForm.tsx
@@ -1,129 +1,127 @@
 import axios from "axios";
+import { ErrorMessage, Field, Form, Formik } from "formik";
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
+import * as Yup from "yup";
 import SensItem from "../types/SensItem";
-import { useFormik } from "formik";
-import * as Yup from 'yup';
-
-/*
-  CLASSE EDITFORM: classe che renderizza automaticamente la struttura HTML della pagina di modifica di un lampione, definendo anche il metodo per la trasmissione dei dati al server. Stile associato a Bootstrap.
-*/
 
 const EditSensForm: React.FC = () => {
+  axios.defaults.baseURL = "http://localhost:5000/api";
   const navigate = useNavigate();
-  const [sens, setSens] = useState<SensItem | null>(null);
-  const { id } = useParams<{ id: string }>(); // Accesso al parametro id passandolo a useParams()
+  const { id: paramId } = useParams<{ id: string }>();
+  const [isLoading, setIsLoading] = useState(true);
+  const [sens, setSens] = useState<SensItem>({
+    id: Number(paramId),
+    iter: "manuale",
+    IP: "",
+    luogo: "",
+    raggio: 0,
+  });
 
   useEffect(() => {
-    fetchData();
-  }, []); //Anche se il compilatore da warning, mantenere l'array vuoto per evitare loop infiniti, dato che
-  //effettua la richiesta ad ogni render della pagina
-
-  const fetchData = async () => {
-    axios.defaults.baseURL = "http://localhost:5000/api";
-    try {
-      const response = await axios.put<SensItem>(`sensori/edit/${id}`);
-      setSens(response.data);
-    } catch (error) {
-      console.error("Error fetching data:", error);
+    if (sens.id !== 0) {
+      axios
+        .get<SensItem>(`/sensori/${sens.id}`)
+        .then((response) => {
+          setSens(response.data);
+          setIsLoading(false);
+        })
+        .catch((err) => console.log(err));
     }
-  };
+  }, [sens.id]);
 
-  const handleSubmit = async (event: React.FormEvent) => {
-    event.preventDefault(); // Evita l'invio di un form di default
-    const formElement = event.target as HTMLFormElement; // Individuo il form nella pagina HTML
-    const formData = new FormData(formElement); // Creo un oggetto che avrà i dati del form
-    const data = Object.fromEntries(formData.entries()); // Creo un oggetto che prende le entries del form
-
-    try {
-      await axios.post("/sensori", data); // Invio dei dati al server
-      navigate("/"); //va aggiunto check, nel caso di errore fornire una pagina di errore
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
   return (
-    <div>
-      {sens ? (
-        <form title="Form per modifica sensore" onSubmit={handleSubmit}>
+    <Formik
+      initialValues={{
+        id: sens.id || 0,
+        iter: sens.iter || "manuale",
+        IP: sens.IP || "",
+        luogo: sens.luogo || "",
+        raggio: sens.raggio || 0,
+      }}
+      validationSchema={Yup.object({
+        luogo: Yup.string()
+          .min(2, "Inserisci almeno 2 caratteri")
+          .required("Campo obbligatorio")
+          .trim(),
+      })}
+      onSubmit={(values, { setSubmitting }) => {
+        const url = `/sensori/edit/${sens.id}`;
+        axios
+          .put(url, values)
+          .then(() => {
+            navigate("/");
+          })
+          .catch((err) => console.log(err))
+          .finally(() => {
+            setSubmitting(false);
+          });
+      }}
+    >
+      <Form>
         <div className="form-group">
           <label htmlFor="id">ID (Automatico)</label>
-          <input
-            className="form-control"
-            type="text"
-            defaultValue="NULL"
-            name="id"
-            value={sens.id}
-            readOnly
-          />
+          <Field name="id" type="text" className="form-control" readOnly />
         </div>
+
         <div className="form-group">
           <label htmlFor="iter">Interazione</label>
-          <select className="form-control" id="iter" name="iter" value={sens.iter === "manuale" ? "Manuale" : "Automatico"}>
-            <option>Manuale</option>
-            <option>Automatico</option>
-          </select>
+          <Field name="iter" as="select" className="form-control">
+            <option value="manuale">Manuale</option>
+            <option value="automatico">Automatico</option>
+          </Field>
           <small id="statusHelp" className="form-text text-muted">
-            Indica se il sensore ha un controllo sui lampioni (automatico) o se i lampioni saranno modificati manualmente da un utente
+            Indica se il sensore ha un controllo sui lampioni (automatico) o se i lampioni saranno modificati manualmente da un utente.
           </small>
         </div>
+
         <div className="form-group">
           <label htmlFor="IP">Indirizzo IP</label>
-          <input
-            type="text"
-            className="form-control"
-            id="IP"
-            placeholder=""
-            name="IP"
-            value={sens.IP}
-          />
+          <Field name="IP" type="text" className="form-control" />
         </div>
+
         <div className="form-group">
-          <label htmlFor="Locazione">Luogo di Installazione</label>
-          <input
-            type="text"
-            className="form-control"
-            id="Locazione"
-            aria-describedby="locazioneHelp"
-            placeholder=""
-            name="luogo"
-            value={sens.luogo}
-          />
+          <label htmlFor="luogo">Luogo di Installazione</label>
+          <Field name="luogo" type="text" className="form-control" />
           <small id="locazioneHelp" className="form-text text-muted">
             Indica il luogo in cui è situato il sensore.
           </small>
+          <ErrorMessage name="luogo" />
         </div>
+
         <div className="form-group">
           <label htmlFor="raggio">Raggio d'azione</label>
-          <select className="form-control" id="raggio" name="raggio" value={sens.raggio}>
-            <option>0</option>
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-            <option>6</option>
-            <option>7</option>
-            <option>8</option>
-            <option>9</option>
-            <option>10</option>
-          </select>
+          <Field name="raggio" as="select" className="form-control">
+            <option value="0">0</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+          </Field>
           <small id="intensityHelp" className="form-text text-muted">
             Indica il raggio d'azione (in metri) del sensore.
           </small>
         </div>
+
         <button type="submit" className="btn btn-primary">
           Modifica
         </button>
         <Link to="/" type="button" className="btn btn-outline-primary">
           Indietro
         </Link>
-      </form>
-      ) : (
-        <p>Nessun dato disponibile</p>
-      )}
-    </div>
+      </Form>
+    </Formik>
   );
 };
+
 export default EditSensForm;
