4d30c68591b2324595af87aa4cb2402a2dd5525d
matnut2 <matteosolda12@gmail.com>
2023-07-11 15:06:45 +0200

    Aggiornamento server/dist/*

--- server/dist/routes/lampioneRoutes.js
+++ server/dist/routes/lampioneRoutes.js
@@ -1,4 +1,7 @@
 "use strict";
+/*
+    Lo scopo di questo script è quello di gestire le routes per le richieste relative ai lampioni in arrivo al server
+*/
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
