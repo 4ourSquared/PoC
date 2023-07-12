74cabcf2fd17b2968425362b692e63554db44b15
EricaCavaliere <erica.cavaliere.euganeoinf@gmail.com>
2023-07-12 16:46:16 +0200

    Pagina di modifica creata da @LSalamiUnipd

--- client/src/components/NewAreaPage.tsx
+++ client/src/components/NewAreaPage.tsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, {Component} from "react";
 import NewAreaForm from "./NewAreaForm";
 import Header from "./Header";
 import Footer from "./Footer";
 
-export const NewAreaPage: React.FC = () => {
-  return (
-    <>
-      <Header />
-      <NewAreaForm />
-      <Footer />
-    </>
-  );
+/*
+  CLASS NEWAREAPAGE: classe che renderizza automaticamente l'intera pagina contenente il form per l'aggiunta di una nuova area illuminata.
+*/
+export default class NewAreaPage extends Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <NewAreaForm />
+        <Footer />
+      </>
+    );
+  }
 };
