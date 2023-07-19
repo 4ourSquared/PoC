d4fcea26c87089989f150e4b90e6fb1fb4a3e52f
EricaCavaliere <erica.cavaliere.euganeoinf@gmail.com>
2023-07-19 16:00:01 +0200

    Aggiornati link router

--- client/src/components/NewSensPage.tsx
+++ client/src/components/NewSensPage.tsx
@@ -6,8 +6,9 @@ import Footer from "./Footer";
 /*
   CLASS NEWSENSPAGE: classe che renderizza automaticamente l'intera pagina contenente il form per l'aggiunta di un nuovo sensore.
 */
-export default class NewSensPage extends Component {
+export default class NewSensPage extends Component<{ areaId: number }> {
   render() {
+    const { areaId } = this.props;
     return (
       <>
         <Header />
